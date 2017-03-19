1ca82c1f96f64298214b7f69c246d9eef6c825cd
Jake <me@jakespringer.com>
2017-03-19 14:32:59 -0700

    Changed OpenGL version to 3.3

--- CardboardBox/src/game/Renderer.java
+++ CardboardBox/src/game/Renderer.java
@@ -58,7 +58,7 @@ public class Renderer {
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
