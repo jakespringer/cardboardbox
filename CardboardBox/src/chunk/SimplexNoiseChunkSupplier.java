5afacc079e5ac10a9aeb5fed14fe4f57e68a3f8f
Jake <me@jakespringer.com>
2017-03-19 14:17:33 -0700

    Added optimization to Simplex Noise supplier

--- CardboardBox/src/chunk/SimplexNoiseChunkSupplier.java
+++ CardboardBox/src/chunk/SimplexNoiseChunkSupplier.java
@@ -8,6 +8,7 @@ import util.VectorKey;
 public class SimplexNoiseChunkSupplier implements ChunkSupplier {
 	private Noise noise;
 	private HashMap<VectorKey, Chunk> cache = new HashMap<>();
+	private Chunk emptyChunk = new Chunk();
 	
 	public SimplexNoiseChunkSupplier(double seed) {
 		noise = new Noise(seed);
@@ -23,14 +24,21 @@ public class SimplexNoiseChunkSupplier implements ChunkSupplier {
 		if (cache.containsKey(key)) {
 			return cache.get(key);
 		} else {
-			Chunk chunk = new Chunk();
+			Chunk chunk = null;
 			for (int i=0; i<Chunk.SIDE_LENGTH; ++i) {
 				for (int j=0; j<Chunk.SIDE_LENGTH; ++j) {
 					int height = (int) Math.floor(noise.fbm(x*Chunk.SIDE_LENGTH+i, z*Chunk.SIDE_LENGTH+j, 4, 100));
 					if ((height / Chunk.SIDE_LENGTH) == y) {
+						if (chunk == null) {
+							chunk = new Chunk();
+						}
 						chunk.setColor(i, height % Chunk.SIDE_LENGTH, j, 0x00AA00);
 					}
 				}
+			}
+			
+			if (chunk == null) {
+				chunk = emptyChunk;
 			}
 			cache.put(key, chunk);
 			return chunk;
